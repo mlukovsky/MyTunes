84ec954ab4cfaad2b70b8edf53cd91a30e8e1207
Mitchell Lukovsky <m.lukovsky2@gmail.com>
2025-11-02 19:48:47 -0500

    chore: clean up

--- helpers/catchAsync.js
+++ helpers/catchAsync.js
@@ -1,7 +1,6 @@
 //Catches errors in async functions and passes them to next
 
 module.exports = func => {
-    //pass in func
     return (req, res, next) => {
         //returns a new function that has func executed, and then catches errors and passes them to next
         func(req, res, next).catch(next);
